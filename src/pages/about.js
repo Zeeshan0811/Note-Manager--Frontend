083289a39ec2d3d0bbc72006fdc50f926e51ee1b
Zeeshan Akhtar <Zeeshan0811@gmail.com>
2025-07-19 01:47:49 +0600

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div className="row mt-5 justify-content-center">
             <div className="col-md-8 text-center">
+                <h2>Welcome</h2>
                 <h4>Note Manager</h4>
                 <hr />
                 <p>Note Manger is a basic note creating app which has CRUD operations. CRUD stands for Create, Read, Update and Delete, and these are the essential functions of every application.</p>
